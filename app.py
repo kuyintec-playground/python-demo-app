7d781fda460016b346306575e89c289fd1119fce
unknown <kuyintec_admin@kuyintec.com>
2021-07-04 21:37:33 +0530

    fix lint issue

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+"""
+python demo app
+"""
 from __future__ import absolute_import
 import os
-from flask import Flask, render_template, request, redirect, session
-
+from flask import Flask, render_template
 app = Flask(__name__, static_folder='assets', static_url_path='')
+
 
 @app.route('/', methods=['GET'])
 def home():
     """ Home Page..
     """
     return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.debug = os.environ.get('FLASK_DEBUG', True)
